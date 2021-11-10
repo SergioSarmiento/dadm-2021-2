2c7e9441eef641480e697e5a61005ac4a137a5c8
Sergio Sarmiento <sisarmientob@unal.edu.co>
2021-11-10 17:56:10 -0500

    Se añade menu de opciones falta logica

--- reto3/tic_tac_toe/lib/assets/constants.dart
+++ reto3/tic_tac_toe/lib/assets/constants.dart
@@ -2,14 +2,17 @@
 import 'package:flutter/material.dart';
 
 const String title = 'Tic-Tac-Toe Game';
-const String playerOneSymbol="X";
-const String playerTwoSymbol="O";
+const String playerOneSymbol = "X";
+const String playerTwoSymbol = "O";
 const String blank = '';
 const String playerName = 'You';
 const String computerName = 'Computer';
 const String draw = 'It\'s a tie';
 const String winText = 'The winner is ';
 const String turnText = 'Turn: ';
+const String difficultyEasy = 'Easy';
+const String difficultyMedium = 'Medium';
+const String difficultyHard = 'Hard';
 //Color
 const Color playerOneColor = Colors.green;
 const Color playerTwoColor = Colors.red;
@@ -18,5 +21,5 @@ const Color borders = Colors.blue;
 const Color winningSet = Colors.greenAccent;
 //numbers
 const double fontSize = 20.0;
-const double cellSize = 22.0;
+const double cellSize = 20.0;
 const double borderWidth = 3;
